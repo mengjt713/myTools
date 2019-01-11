e637177ac289c2fba9e8d43deb458afd38925e25
mengjintao <mengjt713@163.com>
2019-01-11 14:38:01 +0800

    下载文件

---
+++ src/comon/download.js
@@ -0,0 +1,66 @@
+define(function () {
+
+    function downLoadFile(data){
+        var blob = dataURLtoBlob("data:text/csv;base64," +data);
+        if(isIE()){
+            navigator.msSaveBlob(blob, "文件"+formateDate+".xml");
+        }else {
+            var a = document.getElementById("downLog");
+            //noinspection JSDuplicatedDeclaration
+            a.download = '文件'+formateDate()+'.xml';
+            a.href = URL.createObjectURL(blob);
+            a.click();
+        }
+    }
+    /*
+    * 日期格式化
+    * */
+    function formateDate(){
+        var _addSuffix=function(value){
+            value=value < 10 ? '0'+value : value;
+            return value;
+        };
+        var date=new Date();
+        var year=date.getFullYear().toString();
+        var month=_addSuffix(date.getMonth()+1);
+        var day=_addSuffix(date.getDate());
+        var hour=_addSuffix(date.getHours());
+        var minute=_addSuffix(date.getMinutes());
+        var second=_addSuffix(date.getSeconds());
+        return year+month+day+hour+minute+second;
+    }
+    /**
+     * base64转blob
+     */
+    function dataURLtoBlob(dataurl) {
+        var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+            bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+        // var bom = "\ufeff",m= bom.length,bomarr = new Uint8Array(n);
+        while (n--) {
+            u8arr[n] = bstr.charCodeAt(n);
+        }
+        return new Blob([ new Uint8Array([0xEF, 0xBB, 0xBF]),u8arr] , { type: mime+";charset=utf-8,"});
+    }
+
+    /**
+     * 判断浏览器
+     */
+    function isIE() {
+        var agent = window.navigator.userAgent;
+        if(agent.indexOf("Edge") > -1){//首先判断是否为Edge浏览器
+            return true;
+        }
+        else if(agent.indexOf("Firefox") > -1){//判断是否为firefox
+            return false;
+        }
+        else if (agent.indexOf("Chrome") <= -1) {
+            return true;
+        } else {//是否为chrome
+            return false;
+        }
+    }
+    return {
+        downLoadFile:downLoadFile
+    }
+
+});
--- src/comon/encryptAndDecrypt.js
+++ src/comon/encryptAndDecrypt.js
@@ -1,52 +1,60 @@
-/*
-* 解密
-* */
-function decrypt (password) {
-    var newArray = [],
-        oldPassword = password,
-        newPassword = [];
-    oldPassword.split('').forEach(function (data, index) {
-        if (index % 2 == 0) {
-            var dataCode = (parseInt(oldPassword.substring(index, index + 2), 16)) ^ 'u'.charCodeAt();
-            newArray[index / 2] = String.fromCharCode(dataCode);
-        }
-    })
-    newArray.forEach(function (data, index) {
-        if (index % 2) {
-            newPassword[index - 1] = data;
-        } else {
-            if (index == oldPassword.length - 1) {
-                newPassword[index] = data;
+;define(function () {
+
+    /*
+     * 解密
+     * */
+    function decrypt(password) {
+        var newArray = [],
+            oldPassword = password,
+            newPassword = [];
+        oldPassword.split('').forEach(function (data, index) {
+            if (index % 2 == 0) {
+                var dataCode = (parseInt(oldPassword.substring(index, index + 2), 16)) ^ 'u'.charCodeAt();
+                newArray[index / 2] = String.fromCharCode(dataCode);
+            }
+        })
+        newArray.forEach(function (data, index) {
+            if (index % 2) {
+                newPassword[index - 1] = data;
             } else {
-                newPassword[index + 1] = data;
+                if (index == oldPassword.length - 1) {
+                    newPassword[index] = data;
+                } else {
+                    newPassword[index + 1] = data;
+                }
             }
-        }
-    })
-    return newPassword.join('')
-}
-/*
-* 加密
-* */
-function encryption(password) {
-    var newArray= [],
-        oldPassword = password,
-        newPassword = null;
-    oldPassword.split('').forEach(function (data,index) {
-        var dataCode = (data.charCodeAt() ^ 'u'.charCodeAt()).toString(16);
-        if(dataCode.length==1){
-            dataCode = "0"+dataCode
-        }
-        if(index%2){
-            newArray[index-1] = dataCode;
-        }else {
-            if(index == oldPassword.length-1){
-                newArray[index] = dataCode;
-            }else{
-                newArray[index+1] = dataCode;
+        });
+        return newPassword.join('')
+    }
+
+    /*
+     * 加密
+     * */
+    function encryption(password) {
+        var newArray = [],
+            oldPassword = password,
+            newPassword = null;
+        oldPassword.split('').forEach(function (data, index) {
+            var dataCode = (data.charCodeAt() ^ 'u'.charCodeAt()).toString(16);
+            if (dataCode.length == 1) {
+                dataCode = "0" + dataCode
             }
-        }
-    })
-    return newArray.join('')
-}
-var a = encryption("Aa1234")
-console.log(decrypt(a));
+            if (index % 2) {
+                newArray[index - 1] = dataCode;
+            } else {
+                if (index == oldPassword.length - 1) {
+                    newArray[index] = dataCode;
+                } else {
+                    newArray[index + 1] = dataCode;
+                }
+            }
+        });
+        return newArray.join('')
+    }
+
+    return {
+        decrypt: decrypt,
+        encryption: encryption
+    }
+
+});
